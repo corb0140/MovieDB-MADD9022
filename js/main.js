102bc27f202cb3475600ae10044123e0e2a88c9a
Mark Corbin <corb0140@algonquinlive.com>
2024-02-17 12:45:12 -0500

    continuing setup for html pages and service worker

--- js/main.js
+++ js/main.js
@@ -1,12 +1,44 @@
 const APP = {
   searchForm: document.getElementById("search-form"),
   search: document.getElementById("search"),
+  selectOption: document.querySelector(".selectOption"),
+  selection: null,
+  cardContainer: document.querySelector(".card-container"),
   fetchUrl: "https://moviedb-6n0o.onrender.com/",
+  h1: document.querySelector("h1"),
 
-  init: function () {},
+  init: function () {
+    APP.serviceWorker();
 
-  fetchMovies: function (query) {
-    fetch(`${APP.fetchUrl}search?query=${query}`)
+    if (window.location.pathname === "/index.html") {
+      APP.searchForm.addEventListener("submit", (ev) => {
+        ev.preventDefault();
+
+        if (APP.selectOption.value === "popularity") {
+          APP.selection = "popularity";
+        } else if (APP.selectOption.value === "release-date") {
+          APP.selection = "release-date";
+        } else if (APP.selectOption.value === "vote") {
+          APP.selection = "vote";
+        }
+
+        // pass keyword and selection to searchResults.html query
+        APP.searchResults(APP.selection, APP.search.value);
+
+        APP.searchForm.reset();
+      });
+    }
+
+    // Online/Offline
+    APP.online();
+    APP.offline();
+
+    // CONNECTED
+    console.log("CONNECTED");
+  },
+
+  fetchMovies: (selection, keyword) => {
+    fetch(`${APP.fetchUrl}api/${selection}?keyword=${keyword}`)
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
@@ -14,9 +46,85 @@ const APP = {
 
         return response.json();
       })
-      .then((data) => {
+      .then(({ data }) => {
         console.log(data);
+        APP.cardContainer.innerHTML = "";
+
+        console.log(data);
+        let li = document.createElement("li");
+        let list = new DocumentFragment();
+
+        data.forEach((movie) => {
+          li.innerHTML += `
+              <div class="card">
+                  <div class="card-img">
+                  <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+                  </div>
+
+                  <div class="card-content">
+                  <h2>${movie.title}</h2>
+                  </div>
+              </div>
+            `;
+
+          list.appendChild(li);
+        });
+
+        APP.cardContainer.appendChild(list);
       });
+  },
+
+  searchResults: (selection, keyword) => {
+    window.location.href = `searchResults.html?keyword=${keyword}&selection=${selection}`;
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const qKeyword = urlParams.get("keyword");
+    const qSelection = urlParams.get("selection");
+
+    console.log(qKeyword, qSelection);
+    console.log(keyword, selection);
+
+    // fetch movies
+    APP.fetchMovies(selection, keyword);
+  },
+
+  cacheResults: () => {},
+
+  details: () => {},
+
+  handleMovieClick: () => {},
+
+  serviceWorker: () => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("../sw.js").then((registration) => {
+        // console.warn("ServiceWorker registered ", registration);
+      });
+    }
+  },
+
+  sendMessage: (message) => {
+    navigator.serviceWorker.ready.then((reg) => {
+      reg.active.postMessage(message);
+    });
+  },
+
+  receiveMessage: () => {
+    navigator.serviceWorker.addEventListener("message", (ev) => {
+      console.log("Message received from service worker", ev.data);
+    });
+  },
+
+  online: () => {
+    window.addEventListener("online", () => {
+      APP.h1.textContent = "Online";
+    });
+  },
+
+  offline: () => {
+    window.addEventListener("offline", () => {
+      APP.h1.textContent = "Offline";
+    });
   },
 };
 
--- sw.js
+++ sw.js
@@ -4,9 +4,57 @@ const staticAssets = [
   "./",
   "./index.html",
   "./cacheResults.html",
-  "./Error404.html",
+  "./404.html",
   "./searchResults.html",
   "./details.html",
   "./js/main.js",
   "./css/main.css",
 ];
+self.addEventListener("install", (ev) => {
+  // cache static files
+  ev.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(staticAssets);
+    })
+  );
+});
+
+self.addEventListener("activate", async (ev) => {
+  //clear old caches
+  ev.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (ev) => {
+  let url = new URL(ev.request.url);
+  const isJSON = url.hostname.includes("moviedb-6n0o.onrender.com");
+  let isOnline = navigator.onLine; // determine if the browser is currently offline
+  let isImage =
+    url.pathname.includes("png") ||
+    url.pathname.includes("jpg") ||
+    url.pathname.includes("jpeg") ||
+    url.pathname.includes("webp") ||
+    url.pathname.includes("gif") ||
+    url.pathname.includes("svg") ||
+    url.pathname.includes("ico") ||
+    url.hostname.includes("image.tmdb.org");
+
+  if (isOnline) {
+    // if online, fetch the resource
+    // ev.respondWith(fetchOnline(ev));
+  } else {
+    // if offline, respond with cached files
+    // ev.respondWith(fetchOffline(ev));
+  }
+});
+
+const fetchOnline = (ev) => {};
+
+const fetchOffline = (ev) => {};
+
+self.addEventListener("message", (ev) => {});
