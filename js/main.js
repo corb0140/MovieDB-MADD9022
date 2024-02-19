1e736dbbeb22bac16f80a15bc1bce5f912da4045
Mark Corbin <corb0140@algonquinlive.com>
2024-02-19 10:48:36 -0500

    testing github pages

--- js/main.js
+++ js/main.js
@@ -4,13 +4,12 @@ const APP = {
   selectOption: document.querySelector(".selectOption"),
   selection: null,
   cardContainer: document.querySelector(".card-container"),
-  fetchUrl: "https://moviedb-6n0o.onrender.com/",
   h1: document.querySelector("h1"),
 
   init: function () {
     APP.serviceWorker();
 
-    if (window.location.pathname === "/index.html") {
+    if (window.location.pathname === "./index.html") {
       APP.searchForm.addEventListener("submit", (ev) => {
         ev.preventDefault();
 
@@ -21,9 +20,6 @@ const APP = {
         } else if (APP.selectOption.value === "vote") {
           APP.selection = "vote";
         }
-
-        // fetch movies
-        // APP.fetchMovies(APP.selection, APP.search.value);
 
         //  redirect to searchResults.html
         window.location.href = `./searchResults.html?selection=${APP.selection}&keyword=${APP.search.value}`;
@@ -39,45 +35,6 @@ const APP = {
     // CONNECTED
     console.log("CONNECTED");
   },
-
-  fetchMovies: (selection, keyword) => {
-    // fetch(`${APP.fetchUrl}api/${selection}?keyword=${keyword}`)
-    //   .then((response) => {
-    //     if (!response.ok) {
-    //       throw new Error("Network response was not ok");
-    //     }
-    //     return response.json();
-    //   })
-    //   .then(({ data }) => {
-    //     console.log(data);
-    //     APP.cardContainer.innerHTML = "";
-    //     console.log(data);
-    //     let li = document.createElement("li");
-    //     let list = new DocumentFragment();
-    //     data.forEach((movie) => {
-    //       li.innerHTML += `
-    //           <div class="card">
-    //               <div class="card-img">
-    //               <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
-    //               </div>
-    //               <div class="card-content">
-    //               <h2>${movie.title}</h2>
-    //               </div>
-    //           </div>
-    //         `;
-    //       list.appendChild(li);
-    //     });
-    //     APP.cardContainer.appendChild(list);
-    //   });
-  },
-
-  searchResults: () => {},
-
-  cacheResults: () => {},
-
-  details: () => {},
-
-  handleMovieClick: () => {},
 
   serviceWorker: () => {
     if ("serviceWorker" in navigator) {
