8e34a59be57eaef2f7e03296680502039ccfaaef
Mark Corbin <corb0140@algonquinlive.com>
2024-02-19 10:16:47 -0500

    test github pages

--- sw.js
+++ sw.js
@@ -30,80 +30,50 @@ self.addEventListener("activate", async (ev) => {
   );
 });
 
-self.addEventListener("fetch", function (event) {
-  console.log("Fetch event for ", event.request.url);
-  event.respondWith(
-    fetch(event.request).catch(function () {
-      console.error("Fetch failed; returning offline page instead.");
-      return new Response("You are offline.");
-    })
-  );
+//
+
+self.addEventListener("fetch", (ev) => {
+  let mode = ev.request.mode;
+  let url = new URL(ev.request.url);
+  const isAPI = url.hostname.includes("render.com");
+  let isOnline = navigator.onLine; // determine if the browser is currently offline
+  let isImage =
+    url.pathname.includes("png") ||
+    url.pathname.includes("jpg") ||
+    url.pathname.includes("jpeg") ||
+    url.pathname.includes("webp") ||
+    url.pathname.includes("gif") ||
+    url.pathname.includes("svg") ||
+    url.pathname.includes("ico") ||
+    url.hostname.includes("image.tmdb.org");
+  let isSearchResults = url.pathname.includes("searchResults.html");
+  let isDetails = url.pathname.includes("details.html");
+
+  console.log(url);
+  console.log(ev.request);
+  console.log(isAPI);
+
+  if (isOnline) {
+    // if online, fetch the resource
+
+    if (mode === "navigate") {
+      if (isSearchResults) {
+        ev.respondWith(
+          fetch(ev.request).catch(() => {
+            return caches.match("./404.html");
+          })
+        );
+      }
+    }
+  } else {
+    // if offline, respond with cached files
+
+    if (mode === "navigate") {
+      if (isSearchResults) {
+        ev.respondWith(caches.match("./cachesResults.html"));
+      }
+    }
+  }
 });
-
-// self.addEventListener("fetch", (ev) => {
-//   let mode = ev.request.mode;
-//   let url = new URL(ev.request.url);
-//   const isJSON = url.hostname.includes("moviedb-6n0o.onrender.com");
-//   let isOnline = navigator.onLine; // determine if the browser is currently offline
-//   let isImage =
-//     url.pathname.includes("png") ||
-//     url.pathname.includes("jpg") ||
-//     url.pathname.includes("jpeg") ||
-//     url.pathname.includes("webp") ||
-//     url.pathname.includes("gif") ||
-//     url.pathname.includes("svg") ||
-//     url.pathname.includes("ico") ||
-//     url.hostname.includes("image.tmdb.org");
-//   let isSearchResults = url.pathname.includes("searchResults.html");
-//   let isDetails = url.pathname.includes("details.html");
-
-//   console.log(url);
-//   console.log(ev.request);
-//   console.log(isJSON);
-
-//   if (isOnline) {
-//     // if online, fetch the resource
-
-//     if (mode === "navigate") {
-//       if (isSearchResults) {
-//         ev.respondWith(
-//           fetch(ev.request).catch(() => {
-//             return caches.match("./searchResults.html");
-//           })
-//         );
-//       } else if (isDetails) {
-//         ev.respondWith(
-//           fetch(ev.request).catch(() => {
-//             return caches.match("./details.html");
-//           })
-//         );
-//       } else {
-//         ev.respondWith(
-//           fetch(ev.request).catch(() => {
-//             return caches.match("./index.html");
-//           })
-//         );
-//       }
-//     }
-//   } else {
-//     // if offline, respond with cached files
-
-//     if (mode === "navigate") {
-//       if (caches.keys().length === 0) {
-//         ev.respondWith(
-//           fetch(ev.request).catch(() => {
-//             return caches.match("./404.html");
-//           })
-//         );
-//       } else {
-//         ev.respondWith(
-//           fetch(ev.request).catch(() => {
-//             return caches.match("./cacheResults.html");
-//           })
-//         );
-//       }
-//     }
-//   }
-// });
 
 self.addEventListener("message", (ev) => {});
